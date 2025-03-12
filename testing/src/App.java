c0e2983a0614d8c5e3455a5e7dc090cc8bd4385a
lecor <88902282+lecorrr@users.noreply.github.com>
2025-03-12 16:10:29 +0200

    Update App.java

--- testing/src/App.java
+++ testing/src/App.java
@@ -33,8 +33,8 @@ public class App {
         System.out.println("Collected numbers: " + numbers);
         
         int max = 0;
-        for(int j : numbers){
-            if(max < j){
+        for(int j : numbers) {
+            if(max < j) {
                 max = j;
             }
         }
